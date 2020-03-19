1d99c4a920a681259e9ab37c4a497c75b5057fb8
roldie9 <aroldoreyes9@gmail.com>
2020-03-18 22:08:53 -0500

    init

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # eat-da-burger-HW-13
+
+## Description
+
 Eat-Da-Burger! is a restaurant app that lets users input names of burgers and then "Devour It".
+
+## Programs used
+
+MySql, Node, Express, Handlebars and a homemade ORM.
+
+MVC design pattern used.
+
+Node and MySQL used to query and route data in the app.
+
+Handlebars was used to generate the HTML.
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Big Mac');
+INSERT INTO burgers (name) VALUES ('Whopper');
+INSERT INTO burgers (name) VALUES ('Baconator');
+INSERT INTO burgers (name, devoured) VALUES ('White Castle', true);
+INSERT INTO burgers (name, devoured) VALUES ('Krabby Patty', true);
+INSERT INTO burgers (name, devoured) VALUES ('Good Burger', true);
