93977d413c7f9295931f93d75014cc90545c58b4
roldie9 <aroldoreyes9@gmail.com>
2020-03-18 23:52:50 -0500

    created get, post, update routes

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -18,7 +18,7 @@ router.get("/", function(req, res) {
 
 router.post("/api/burgers", function(req, res) {
   burger.create([
-    "name", "sleepy"
+    "name", "devoured"
   ], [
     req.body.name, req.body.sleepy
   ], function(result) {
@@ -33,7 +33,7 @@ router.put("/api/burgers/:id", function(req, res) {
   console.log("condition", condition);
 
   burger.update({
-    sleepy: req.body.sleepy
+    devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
